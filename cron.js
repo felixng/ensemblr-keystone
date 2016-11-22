3cdecd87b376dd9d6eeb7f984627efd521164f4e
felix.ng <nfelix@gmail.com>
2016-11-22 23:22:35 +0000

    refactored

--- cron.js
+++ cron.js
@@ -1,3 +1,4 @@
+var keystone = require('keystone');
 // var mongoose = require('mongoose'),
 //     keystone = require('keystone');
 
@@ -25,34 +26,109 @@ var cron = module.exports = {};
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
-var getEntity = function(){
-  console.log('tick');
+var createTheatre = function(name, url){
+    keystone.createItems({
+        Theatre: [{
+            name: name,
+            sourceUrl: url,
+        }]
+    }, function(err, stats) {
+        stats && console.log(stats.message);
+        // done(err);
+    });
+}
+
+var getTheatres = function(){
+  console.log('getTheatres');
   //var url = 'https://data.import.io/extractor/cd74f836-cdbd-4ff1-a327-bdb31f882fc9/json/latest?_apikey=' + process.env.IMPORTIO_API;
-  var url = 'https://extraction.import.io/query/extractor/cd74f836-cdbd-4ff1-a327-bdb31f882fc9?_apikey=' + process.env.IMPORTIO_API;
+  // var url = 'https://extraction.import.io/query/extractor/cd74f836-cdbd-4ff1-a327-bdb31f882fc9?_apikey=' + process.env.IMPORTIO_API;
+  var url = process.env.THEATRE_IMPORTER_URL;
 
   client.get(url, function (data, response) {
       // parsed response body as js object
       // var entity = JSON.parse(data[0].name);
       //console.log(data);
 
-      var entities = data.extractorData.data[0].group;
-      var count = Object.keys(entities).length;
-      
-      for (var i = 0; i < count; i++) {
-          //TODO: Add to keystone
-      };
+      var theatres = data.extractorData.data[0].group;
+      var count = Object.keys(theatres).length;
 
-      //console.log(data.extractorData.data[0].group[0].Name);
-      //console.log(data.extractorData.data[0].group[1].Name);
+      for (var i = 0; i < count; i++) {
+          var found = false;
+          var theatreName = theatres[i].Name[0].text;
+          var theatreUrl = theatres[i].Name[0].href;
+          var Theatre = keystone.list('Theatre');
+          var query = Theatre.model.find().where('name', theatreName);
+
+          query.exec(function(err, results) {
+            found = true;
+          });
+
+          if (!found)  {
+            createTheatre(theatreName, theatreUrl);  
+          }
+      };
 
   });
 }
 
-cron.job = function(){
+var getShows = function(){
+  console.log('getTheatres');
+  //var url = 'https://data.import.io/extractor/cd74f836-cdbd-4ff1-a327-bdb31f882fc9/json/latest?_apikey=' + process.env.IMPORTIO_API;
+  //var url = process.env.THEATRE_IMPORTER_URL;
+  var url = process.env.SHOW_IMPORTER_URL;
+
+  client.get(url, function (data, response) {
+      // parsed response body as js object
+      // var entity = JSON.parse(data[0].name);
+      //console.log(data);
+
+      var shows = data.extractorData.data[0].group;
+      var count = Object.keys(shows).length;
+      
+      // console.log(shows[0]);
+      // console.log(shows[0].Name[0].text);
+
+      for (var i = 0; i < count; i++) {
+
+          var theatreName = shows[i].Name[0].text;
+          var theatreUrl = shows[i].Name[0].href;
+          var Theatre = keystone.list('Theatre');
+          console.log(keystone.model.find().where('name').equal(theatreName));
+
+          // keystone.createItems({
+          //     Theatre: [{
+          //         name: theatreName,
+          //         sourceUrl: theatreUrl,
+          //     }]
+          // }, function(err, stats) {
+          //     stats && console.log(stats.message);
+          //     // done(err);
+          // });
+      };
+
+  });
+}
+
+cron.theatres = function(){
     var job = new CronJob({
       cronTime: '*/10 * * * * *',
       onTick: function(){
-          getEntity();
+          getTheatres();
+      },
+      onComplete: function () {
+        /* This function is executed when the job stops */
+      },
+      start: false
+    });
+
+    job.start();
+}
+
+cron.shows = function(){
+    var job = new CronJob({
+      cronTime: '*/10 * * * * *',
+      onTick: function(){
+          getShows();
       },
       onComplete: function () {
         /* This function is executed when the job stops */
